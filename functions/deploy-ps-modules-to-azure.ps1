d230726d90a0f91ca685d81743520262ea9ca02e
ethorneloe <eric.thorneloe@daf.qld.gov.au>
2024-07-17 20:52:27 +0800

    Use a function instead of a script file, fix up tests, use write-information to toggle output, use output object for automated testing.

---
+++ functions/deploy-ps-modules-to-azure.ps1
@@ -0,0 +1,165 @@
+<#
+.SYNOPSIS
+    Deploys PowerShell modules as zip files to Azure Storage.
+
+.DESCRIPTION
+    This script zips each PowerShell module located in a specified repository path,
+    then uploads the zip files to a specified Azure Storage container. It checks each module
+    for valid .psd1 and .psm1 files and reads version information from the .psd1 file for naming the zip files.
+
+.PARAMETER moduleSourcePath
+    Path containing the PowerShell module folders.
+
+.PARAMETER outputPath
+    Path for compressed module folders, and AzCopy logs.
+
+.PARAMETER storageAccountContainerName
+    Name of the Azure Storage container where the zipped modules will be stored.
+
+.PARAMETER storageAccountName
+    Name of the Azure Storage account.
+
+.PARAMETER tenantId
+    Azure tenant ID used for AzCopy authentication.
+
+.PARAMETER overwrite
+    Specifies whether to overwrite existing files in Azure Storage. Accepted values: 'true', 'false'.
+
+.EXAMPLE
+    .\deploy-ps-modules-to-azure.ps1 -moduleSourcePath '.\modules' -outputPath 'C:\temp\deploy-ps-modules-to-azure' -storageAccountContainerName 'psmodules' -storageAccountName 'examplestorage' -tenantId 'your-tenant-id' -overwrite 'false'
+    Deploys all PowerShell modules from the '.\modules' directory to the 'psmodules' container in the 'examplestorage' Azure Storage account.
+
+.NOTES
+    Ensure that Azure CLI and AzCopy are present.
+
+    This script is intended to be used as part of a GitHub composite action.  It is designed to execute after the actions/checkout and azure/login
+    GitHub actions have been executed as it leverages the repo structure and the existing Azure CLI logon context.
+#>
+
+function Deploy-PsModulesToAzure {
+
+    [CmdletBinding(SupportsShouldProcess = $true)]
+
+    param (
+
+        [Parameter(Mandatory = $true)]
+        [string]$moduleSourcePath,
+
+        [Parameter(Mandatory = $true)]
+        [string]$outputPath,
+
+        [Parameter(Mandatory = $true)]
+        [string]$storageAccountContainerName,
+
+        [Parameter(Mandatory = $true)]
+        [string]$storageAccountName,
+
+        [Parameter(Mandatory = $true)]
+        [string]$tenantId,
+
+        [Parameter(Mandatory = $false)]
+        [boolean]$overwrite = $false
+    )
+
+    # Used with automated testing.
+    $outputObject = [PSObject]@{
+        moduleSourcePath = $moduleSourcePath
+        outputPath       = $outputPath
+        overwrite        = $overwrite
+        moduleFolders    = $null
+    }
+
+    Write-Information "Using module source path: $moduleSourcePath"
+    Write-Information "Overwrite set to: $overwrite"
+    $outputObject.overwrite = $overwrite
+
+    # Initialize an ArrayList to store module folders
+    $moduleFolders = [System.Collections.ArrayList]@()
+
+    # Get all directories recursively
+    $directories = Get-ChildItem -Path $moduleSourcePath -Directory -Recurse
+
+    # Check for folders that contain .psd1 and .psm1 files as a basic module check
+    foreach ($directory in $directories) {
+
+        $psd1File = Get-ChildItem -Path $directory.FullName -Filter *.psd1 -ErrorAction SilentlyContinue
+        $psm1File = Get-ChildItem -Path $directory.FullName -Filter *.psm1 -ErrorAction SilentlyContinue
+
+        if ($psd1File -and $psm1File) {
+            # Add the directory to the ArrayList
+            [void]$moduleFolders.Add($directory.FullName)
+        }
+    }
+
+    if ($moduleFolders.count -eq 0) {
+        throw "No valid powershell modules found in this repo. Module folders need to contain .psm1 and .psd1 files"
+    }
+
+    Write-Information "Found module folders:"
+    Write-Information $moduleFolders
+
+    $outputObject.moduleFolders = $moduleFolders
+
+    Write-Information "Using temp path: $outputPath"
+
+    # Create required directories if not present otherwise clean what is inside them.
+    $moduleOutputPath = "$outputPath/modules"
+    $logOutputPath = "$outputPath/logs"
+    $planOutputPath = "$outputPath/plan"
+    $paths = @($moduleOutputPath, $logOutputPath, $planOutputPath)
+    foreach ($path in $paths) {
+        if (-not (Test-Path $path)) {
+            New-Item -Path $path -Type Directory -Force -WhatIf:$false | Out-Null
+        }
+        else {
+            Get-ChildItem $path -Recurse | Remove-Item -Force -Recurse -Confirm:$false -WhatIf:$false
+        }
+    }
+
+    # Check each module for a .psd1 file that contains a version number and if so versioned zip to temp path
+    foreach ($moduleFolder in $moduleFolders) {
+        try {
+            # Find the .psd1 file in the module directory.  This exists as we already tested for it earlier.
+            $psd1File = Get-ChildItem -Path $moduleFolder | Where-Object { $_.extension -eq '.psd1' }
+            $moduleName = $psd1File.BaseName
+
+            # Read the module version from the .psd1 file. Fail here if not found.
+            $content = Get-Content -Path $psd1File.FullName
+            $versionLine = $content | Select-String -Pattern "ModuleVersion\s*=\s*'(\d+\.\d+\.\d+)'"
+            if ($null -eq $versionLine) {
+                throw "ModuleVersion is not present or not set correctly in the .psd1 file.  Expected format is ModuleVersion = 'x.y.z'"
+            }
+
+            # Extract the version number from the psd1 file
+            $moduleVersion = $versionLine.Matches.Groups[1].Value.Trim()
+
+            # Configure filename and path for compression
+            $zipFileName = "$moduleName-v$moduleVersion.zip"
+            $zipFilePath = "$moduleOutputPath/$zipFileName"
+
+            Write-Information "Found $moduleName version $moduleVersion"
+            Write-Information "Creating zip archive: $zipFilePath"
+
+            # Create zip archive in temp folder
+            Compress-Archive -Path "$moduleFolder/*" -DestinationPath $zipFilePath -Force
+        }
+        catch {
+            throw "Unable to complete deployment for module $moduleName. $_"
+        }
+    }
+
+    # Configure env vars for using the Azure CLI OAuth token from the azure/login action, and for redirecting logs files.
+    $Env:AZCOPY_AUTO_LOGIN_TYPE = "AZCLI"
+    $Env:AZCOPY_TENANT_ID = $tenantId
+    $Env:AZCOPY_LOG_LOCATION = $logOutputPath
+    $Env:AZCOPY_JOB_PLAN_LOCATION = $planOutputPath
+
+    # Copy the versioned zip files to Azure and compress log file for artifact upload later on.
+    # Files are cleaned up in another step in action.yml once artifact upload is complete.
+    if ($PSCmdlet.ShouldProcess("Azure storage account", "Upload files")) {
+        Write-Information "Copying zip archives from $moduleOutputPath to Azure storage"
+        azcopy copy "$moduleOutputPath/*" "https://$storageAccountName.blob.core.windows.net/$storageAccountContainerName" --overwrite=$overwrite
+    }
+
+    return $outputObject
+}
--- tests/deploy-ps-modules-to-azure.tests.ps1
+++ tests/deploy-ps-modules-to-azure.tests.ps1
@@ -3,79 +3,108 @@ BeforeAll {
     # Setup test environment
     $now = Get-Date
     $dateTimeString = $now.ToString("yyyy-MM-dd-HH-mm-ss-fff")
-    $scriptPath = $MyInvocation.MyCommand.Path
-    $scriptNameWithoutExtension = [System.IO.Path]::GetFileNameWithoutExtension($scriptPath)
-    $tempFolderName = "$($scriptNameWithoutExtension)_$($dateTimeString)"
+    $tempFolderName = "deploy-ps-modules-to-azure_$($dateTimeString)"
     $tempBasePath = [System.IO.Path]::GetTempPath()
     $tempTestPath = [System.IO.Path]::Combine($tempBasePath, $tempFolderName)
     New-Item -Path $tempTestPath -type Directory | Out-Null
+    $tempModuleSourcePath = "$tempTestPath\Modules"
+    $tempOutputPath = "$tempTestPath\Output"
+    New-item -Path $tempModuleSourcePath -type Directory | Out-Null
+    New-item -Path $tempOutputPath -type Directory | Out-Null
 
     # Create valid module folder
-    $validModulePath = Join-Path -Path $tempTestPath -ChildPath "ValidModule"
+    $validModulePath = Join-Path -Path $tempModuleSourcePath -ChildPath "ValidModule"
     New-Item -Path $validModulePath -ItemType Directory -Force | Out-Null
     New-Item -Path (Join-Path -Path $validModulePath -ChildPath "ValidModule.psd1") -ItemType File -Force -Value "ModuleVersion = '1.1.0'" | Out-Null
     New-Item -Path (Join-Path -Path $validModulePath -ChildPath "ValidModule.psm1") -ItemType File -Force | Out-Null
 
     # Create module folder with only .psm1
-    $onlyPsm1ModulePath = Join-Path -Path $tempTestPath -ChildPath "OnlyPsm1Module"
+    $onlyPsm1ModulePath = Join-Path -Path $tempModuleSourcePath -ChildPath "OnlyPsm1Module"
     New-Item -Path $onlyPsm1ModulePath -ItemType Directory -Force | Out-Null
     New-Item -Path (Join-Path -Path $onlyPsm1ModulePath -ChildPath "OnlyPsm1Module.psm1") -ItemType File -Force | Out-Null
 
     # Create module folder with only .psd1
-    $onlyPsd1ModulePath = Join-Path -Path $tempTestPath -ChildPath "OnlyPsd1Module"
+    $onlyPsd1ModulePath = Join-Path -Path $tempModuleSourcePath -ChildPath "OnlyPsd1Module"
     New-Item -Path $onlyPsd1ModulePath -ItemType Directory -Force | Out-Null
     New-Item -Path (Join-Path -Path $onlyPsd1ModulePath -ChildPath "OnlyPsd1Module.psd1") -ItemType File -Force -Value "ModuleVersion = '1.1.0'" | Out-Null
 
     # Create invalid .psd1 file without ModuleVersion
-    $invalidPsd1ModulePath = Join-Path -Path $tempTestPath -ChildPath "InvalidPsd1Module"
+    $invalidPsd1ModulePath = Join-Path -Path $tempModuleSourcePath -ChildPath "InvalidPsd1Module"
     New-Item -Path $invalidPsd1ModulePath -ItemType Directory -Force | Out-Null
     New-Item -Path (Join-Path -Path $invalidPsd1ModulePath -ChildPath "InvalidPsd1Module.psd1") -ItemType File -Force -Value "NoVersionInfo = '1.1.0'" | Out-Null
     New-Item -Path (Join-Path -Path $invalidPsd1ModulePath -ChildPath "InvalidPsd1Module.psm1") -ItemType File -Force | Out-Null
 
-    # Mocking commands to simulate their behavior without actual execution
-    Mock -CommandName azcopy
+    # Dot source in the function
+    . ".\functions\deploy-ps-modules-to-azure.ps1"
+
+    # Params for the script executions
+    $script:params = @{
+        moduleSourcePath            = $tempModuleSourcePath
+        outputPath                  = $tempOutputPath
+        storageAccountContainerName = 'psmodules'
+        storageAccountName          = 'examplestorage'
+        tenantId                    = 'test'
+        overwrite                   = $false
+    }
 }
 
 Describe "deploy-ps-modules-to-azure.ps1" {
-    $params = @{
-        moduleSourcePath            = $tempTestPath
-        outputPath                  = 'test'
-        storageAccountContainerName = 'psmodules'
-        storageAccountName          = 'examplestorage'
-        tenantId                    = 'your-tenant-id'
-        overwrite                   = 'false'
+
+    BeforeEach {
+
+        # Clear the temp test path containing the modules and archives created in each test
+        if (Test-Path $tempModuleSourcePath ) { Get-ChildItem $tempModuleSourcePath | Remove-Item -Recurse -Force -Confirm:$false }
+        if (Test-Path $tempOutputPath ) { Get-ChildItem $tempOutputPath | Remove-Item -Recurse -Force -Confirm:$false }
     }
 
-    It "should set overwrite to false if null or empty" {
-        $params.overwrite = ''
-        .\scripts\deploy-ps-modules-to-azure.ps1 @params
-        (Get-Variable overwrite -ValueOnly) | Should -Be 'false'
+    It "should throw an error if there are no valid module folders" {
+
+        # Create module folder with only .psm1
+        $onlyPsm1ModulePath = Join-Path -Path $tempModuleSourcePath -ChildPath "OnlyPsm1Module"
+        New-Item -Path $onlyPsm1ModulePath -ItemType Directory -Force | Out-Null
+        New-Item -Path (Join-Path -Path $onlyPsm1ModulePath -ChildPath "OnlyPsm1Module.psm1") -ItemType File -Force | Out-Null
+
+        { Deploy-PsModulesToAzure @params -WhatIf } | Should -Throw "No valid powershell modules found in this repo. Module folders need to contain .psm1 and .psd1 files"
+
+        # Create module folder with only .psd1
+        $onlyPsd1ModulePath = Join-Path -Path $tempModuleSourcePath -ChildPath "OnlyPsd1Module"
+        New-Item -Path $onlyPsd1ModulePath -ItemType Directory -Force | Out-Null
+        New-Item -Path (Join-Path -Path $onlyPsd1ModulePath -ChildPath "OnlyPsd1Module.psd1") -ItemType File -Force -Value "ModuleVersion = '1.1.0'" | Out-Null
+
+        { Deploy-PsModulesToAzure @params -WhatIf } | Should -Throw "No valid powershell modules found in this repo. Module folders need to contain .psm1 and .psd1 files"
+
+        Remove-Item -Path "$tempModuleSourcePath/*" -Recurse -Force -Confirm:$false
+        { Deploy-PsModulesToAzure @params -WhatIf } | Should -Throw "No valid powershell modules found in this repo. Module folders need to contain .psm1 and .psd1 files"
     }
 
-    It "should throw an error if overwrite is not 'true' or 'false'" {
-        $params.overwrite = 'invalid'
-        { .\scripts\deploy-ps-modules-to-azure.ps1 @params } | Should -Throw "The overwrite input when set must be configured as 'true' or 'false' (default is 'false')"
-    }
+    It "should throw an error if a module does not contain a valid module version key/value pair in the .psd1 file" {
 
-    It "should find only valid module folders with both .psd1 and .psm1 files containing valid ModuleVersion .psd1 key" {
-        .\scripts\deploy-ps-modules-to-azure.ps1 @params
-        (Get-Variable moduleFolders -ValueOnly).Count | Should -Be 1
-        (Get-Variable moduleFolders -ValueOnly)[0] | Should -Be $validModulePath
-    }
+        # Create invalid .psd1 file without ModuleVersion
+        $invalidPsd1ModulePath = Join-Path -Path $tempModuleSourcePath -ChildPath "InvalidPsd1Module"
+        New-Item -Path $invalidPsd1ModulePath -ItemType Directory -Force | Out-Null
+        New-Item -Path (Join-Path -Path $invalidPsd1ModulePath -ChildPath "InvalidPsd1Module.psd1") -ItemType File -Force -Value "NoVersionInfo = '1.1.0'" | Out-Null
+        New-Item -Path (Join-Path -Path $invalidPsd1ModulePath -ChildPath "InvalidPsd1Module.psm1") -ItemType File -Force | Out-Null
 
-    It "should throw an error if no valid modules are found" {
-        Remove-Item -Path "$tempTestPath\ValidModule" -Recurse -Force
-        { .\scripts\deploy-ps-modules-to-azure.ps1 @params } | Should -Throw "No valid powershell modules found in this repo. Module folders need to contain .psm1 and .psd1 files"
+        { Deploy-PsModulesToAzure @params -WhatIf } | Should -Throw "Unable to complete deployment for module InvalidPsd1Module. ModuleVersion is not present or not set correctly in the .psd1 file.  Expected format is ModuleVersion = 'x.y.z'"
     }
 
     It "should read module version from .psd1 file and create zip archive" {
-        .\scripts\deploy-ps-modules-to-azure.ps1 @params
-        Assert-MockCalled -CommandName Compress-Archive -Exactly -Times 1
-    }
 
-    It "should configure and call azcopy to upload files" {
-        .\scripts\deploy-ps-modules-to-azure.ps1 @params
-        Assert-MockCalled -CommandName azcopy -Exactly -Times 1
+        if (Test-Path $tempModuleSourcePath ) { Get-ChildItem $tempModuleSourcePath | Remove-Item -Recurse -Force -Confirm:$false }
+        if (Test-Path $tempOutputPath ) { Get-ChildItem $tempOutputPath | Remove-Item -Recurse -Force -Confirm:$false }
+
+        #Create a valid module
+        $validModulePath = Join-Path -Path $tempModuleSourcePath -ChildPath "ValidModule"
+        New-Item -Path $validModulePath -ItemType Directory -Force | Out-Null
+        New-Item -Path (Join-Path -Path $validModulePath -ChildPath "ValidModule.psd1") -ItemType File -Force -Value "ModuleVersion = '1.1.0'" | Out-Null
+        New-Item -Path (Join-Path -Path $validModulePath -ChildPath "ValidModule.psm1") -ItemType File -Force | Out-Null
+
+        Deploy-PsModulesToAzure @params -WhatIf
+        $zipFiles = Get-ChildItem -Path $tempOutputPath -recurse -Filter *.zip
+        $zipFiles | Measure-Object | Select-Object -ExpandProperty Count | Should -BeGreaterThan 0
+        foreach ($zipfile in $zipFiles) {
+            $zipFile | Select-Object -ExpandProperty BaseName | Select-String -Pattern "\d+\.\d+\.\d+" | Should -Not -Be $null
+        }
     }
 }
 
